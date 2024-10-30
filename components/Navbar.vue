c53667bd6b8ea43e64be8616657e8e30d6d0ca8f
Syahrul Safarila <sysafarila.official@gmail.com>
2024-10-30 08:07:40 +0700

    use NuxtImage

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@ const route = useRoute();
     <nav class="bg-white border-b w-full p-2">
         <div class="max-w-screen-md mx-auto flex gap-4 items-center">
             <NuxtLink class="" to="/">
-                <img src="/pokemon.png" class="w-20" alt="logo" />
+                <NuxtImg src="/pokemon.png" class="w-20" alt="logo" />
             </NuxtLink>
             <NuxtLink :class="`hover:text-black ${route.path == '/' ? 'text-black font-semibold' : 'text-gray-600'}`"to="/">
                 Home
