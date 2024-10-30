cff67f4b09ec9bb4bdb9abc4e0a9256f3602c4d7
Syahrul Safarila <sysafarila.official@gmail.com>
2024-10-30 08:04:25 +0700

    add pokemon logo on navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,17 +3,20 @@ const route = useRoute();
 </script>
 
 <template>
-    <nav class="bg-white p-5 border-b w-full">
-        <div class="max-w-screen-md mx-auto flex gap-4">
-            <NuxtLink :class="`text-gray-600 hover:text-black ${route.path == '/' ? 'text-black font-semibold' : ''}`"
-                to="/">Home
+    <nav class="bg-white border-b w-full p-2">
+        <div class="max-w-screen-md mx-auto flex gap-4 items-center">
+            <NuxtLink class="" to="/">
+                <img src="/pokemon.png" class="w-20" alt="logo" />
             </NuxtLink>
-            <NuxtLink
-                :class="`text-gray-600 hover:text-black ${route.path == '/favorite' ? 'text-black font-semibold' : ''}`"
-                to="/favorite">Favorite</NuxtLink>
-            <NuxtLink
-                :class="`text-gray-600 hover:text-black ${route.path == '/abilities' ? 'text-black font-semibold' : ''}`"
-                to="/abilities">Abilities</NuxtLink>
+            <NuxtLink :class="`hover:text-black ${route.path == '/' ? 'text-black font-semibold' : 'text-gray-600'}`"to="/">
+                Home
+            </NuxtLink>
+            <NuxtLink :class="`hover:text-black ${route.path == '/favorite' ? 'text-black font-semibold' : 'text-gray-600'}`" to="/favorite">
+                Favorite
+            </NuxtLink>
+            <NuxtLink :class="`hover:text-black ${route.path == '/abilities' ? 'text-black font-semibold' : 'text-gray-600'}`" to="/abilities">
+                Abilities
+            </NuxtLink>
         </div>
     </nav>
 </template>
